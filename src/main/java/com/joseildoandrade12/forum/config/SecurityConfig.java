d63799f105ce9ed85da59f5be3be81671fd95384
joseildoandrade12 <joseildoandrade1405@gmail.com>
2026-07-08 22:58:40 -0300

    fix: correction url vercel forum-frontend

--- src/main/java/com/joseildoandrade12/forum/config/SecurityConfig.java
+++ src/main/java/com/joseildoandrade12/forum/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "forum-frontend-swart.vercel.app"
+                "https://forum-frontend-swart.vercel.app/login"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
